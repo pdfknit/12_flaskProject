8d3248447c66fa4c53bdcfef38b565b402ac6af4
Yasenkova Sofia <yasenkovasofia@gmail.com>
2023-02-27 15:44:47 +0000

    yasenkova_lesson_03

--- user/views.py
+++ user/views.py
@@ -1,24 +1,26 @@
 from flask import Blueprint, render_template
+from flask_login import login_manager
 from werkzeug.exceptions import NotFound
-
-from constants import USERS
+from models import User
 
 user = Blueprint(name='user', import_name=__name__, static_folder='../static', url_prefix='/users')
 
 
 @user.route('/')
 def user_list():
+    users = User.query.all()
+    print('!!!!', users)
     return render_template('user/users_list.html',
-                           users=USERS
+                           users=users
                            )
 
 
 @user.route('/<int:pk>')
 def get_user(pk: int):
     try:
-        user = USERS[pk]
+        cur_user = User.query.filter_by(id=pk).one_or_none()
     except KeyError:
         raise NotFound(f'User {pk} is not found')
     return render_template('user/user_details.html',
-                           username=user
+                           user=cur_user
                            )
--- views.py
+++ views.py
@@ -1,11 +1,17 @@
 from flask import Blueprint, render_template
+
+from auth.views import login_manager
 from constants import ARTICLES, USERS
+from models import User
+from user.views import get_user
 
 main_page = Blueprint(name='main_page', import_name=__name__, static_folder='../static', url_prefix='')
 
 
+
 @main_page.route('/')
 def main():
+    curr_user = 0
     return render_template('/base.html',
-                           articles=ARTICLES, users=USERS
+                           articles=ARTICLES, users=USERS, curr_user=curr_user,
                            )
