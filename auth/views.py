6bffacade9931a8b65a0ee7b33af2d966855d7d6
Yasenkova Sofia <yasenkovasofia@gmail.com>
2023-03-10 11:12:05 +0000

    yasenkova_lesson_05

--- auth/views.py
+++ auth/views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from flask_login import LoginManager, login_user, logout_user, login_required
+from flask import Blueprint, render_template, request, redirect, url_for, current_app
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from sqlalchemy.exc import IntegrityError
 
+from forms.user import RegistrationForm, LoginForm
 from models import User
+from models.database import db
+from werkzeug.exceptions import NotFound
 
 auth_app = Blueprint("auth_app", __name__)
 login_manager = LoginManager()
@@ -25,10 +29,14 @@ __all__ = [
 ]
 
 
-@auth_app.route("/login/", methods=["GET", "POST"], endpoint="login")
-def login():
+@auth_app.route("/login-as/", methods=["GET", "POST"], endpoint="login-as")
+def login_as():
+    if not (current_user.is_authenticated and current_user.is_staff):
+        # non-admin users should not know about this feature
+        raise NotFound
+
     if request.method == "GET":
-        return render_template("auth/login.html")
+        return render_template("auth/login-as.html")
     username = request.form.get("username")
 
     if not username:
@@ -54,3 +62,59 @@ def logout():
 @login_required
 def secret_view():
     return "Super secret data"
+
+
+@auth_app.route("/register/", methods=["GET", "POST"], endpoint="register")
+def register():
+    if current_user.is_authenticated:
+        return redirect("index")
+    error = None
+    form = RegistrationForm(request.form)
+
+    if request.method == "POST" and form.validate_on_submit():
+        if User.query.filter_by(username=form.username.data).count():
+            form.username.errors.append("username already exists!")
+            return render_template("auth/register.html", form=form)
+        if User.query.filter_by(email=form.email.data).count():
+            form.email.errors.append("email already exists!")
+            return render_template("auth/register.html", form=form)
+        user = User(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            username=form.username.data,
+            email=form.email.data,
+            is_staff=False,
+        )
+        user.password = form.password.data
+        db.session.add(user)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            current_app.logger.exception("Could not create user!")
+            error = "Could not create user!"
+        else:
+            current_app.logger.info("Created user %s", user)
+            login_user(user)
+            return redirect(url_for("index"))
+    return render_template("auth/register.html", form=form, error=error)
+
+
+@auth_app.route("/login/", methods=["GET", "POST"], endpoint="login")
+def login():
+    if current_user.is_authenticated:
+        return redirect("index")
+    form = LoginForm(request.form)
+
+    if request.method == "POST" and form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).one_or_none()
+
+        if user is None:
+            return render_template("auth/login.html", form=form, error="username doesn't exist")
+
+        if not user.validate_password(form.password.data):
+            return render_template("auth/login.html", form=form, error="invalid username or password")
+
+        login_user(user)
+        return redirect(url_for("index"))
+
+    return render_template("auth/login.html", form=form)
--- models/user.py
+++ models/user.py
@@ -1,14 +1,30 @@
 from flask_login import UserMixin
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, LargeBinary
 
 from models.database import db
+from security import flask_bcrypt
 
 
 class User(db.Model, UserMixin):
     id = Column(Integer, primary_key=True)
     username = Column(String(80), unique=True, nullable=False)
-    email = Column(String(255), nullable=False, default="", server_default="")
+    email = Column(String(255), unique=True, nullable=False, default="", server_default="")
     is_staff = Column(Boolean, nullable=False, default=False)
+    _password = Column(LargeBinary, nullable=True)
+
+    first_name = Column(String(120), unique=False, nullable=False, default="", server_default="")
+    last_name = Column(String(120), unique=False, nullable=False, default="", server_default="")
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        self._password = flask_bcrypt.generate_password_hash(value)
+
+    def validate_password(self, password) -> bool:
+        return flask_bcrypt.check_password_hash(self._password, password)
 
 
 def __repr__(self):
